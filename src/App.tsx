1f4f948d2f2d1489d5473e13eff713fbace0baef
Vimalate <1902644369@qq.com>
2023-04-29 22:28:12 +0800

    feat：添加路由

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './App.module.css'
 import { BrowserRouter, Route } from "react-router-dom";
 import { HomePage } from "./pages";
+
 function App() {
   return (
     <div className={styles.App}>
