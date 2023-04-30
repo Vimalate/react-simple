0693f932ee96ae801617c49f4a4c98b6236de4aa
Vimalate <1902644369@qq.com>
2023-04-30 15:25:06 +0800

    feat：引入国际化i18n

--- src/redux/languageReducer.ts
+++ src/redux/languageReducer.ts
@@ -1,16 +1,29 @@
+import i18n from 'i18next'
+
 export interface LanguageState {
-  language: "en" | "zh";
-  languageList: { name: string; code: string }[];
+  language: 'en' | 'zh'
+  languageList: { name: string; code: string }[]
 }
 
 const defaultState: LanguageState = {
-  language: "zh",
+  language: 'zh',
   languageList: [
-    { name: "中文", code: "zh" },
-    { name: "English", code: "en" },
+    { name: '中文', code: 'zh' },
+    { name: 'English', code: 'en' },
   ],
-};
+}
 
 export default (state = defaultState, action) => {
-  return state;
-};
+  switch (action.type) {
+    case 'change_language':
+      i18n.changeLanguage(action.payload)
+      return { ...state, language: action.payload }
+    case 'add_language':
+      return {
+        ...state,
+        languageList: [...state.languageList, action.payload],
+      }
+    default:
+      return state
+  }
+}
